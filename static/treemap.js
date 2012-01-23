8d2db931f208597c4f9589740fd505388ee56c13
Adam Hinz <ahinz@azavea.com>
2012-01-23 12:27:43 -0800

    sf updates for non-root mounting

--- static/treemap.js
+++ static/treemap.js
@@ -89,7 +89,7 @@ var tm = {
         $("#logo").click(function() {
         //    location.href="/home";
         });        
-        jQuery.getJSON('/species/json/', function(species){
+        jQuery.getJSON(tm_static + '/species/json/', function(species){
             tm.speciesData = species;
             tm.setupAutoComplete($('#species_search_input')).result(function(event, item) {
                 $("#species_search_id").val(item.id).change(); 
@@ -107,7 +107,7 @@ var tm = {
                 tm.speciesDataListeners[i]();
             }    
         });
-        jQuery.getJSON('/neighborhoods/', {format:'json', list: 'list'}, function(nbhoods){
+        jQuery.getJSON(tm_static + '/neighborhoods/', {format:'json', list: 'list'}, function(nbhoods){
             tm.locations = nbhoods;
             tm.setupLocationList();
         });
@@ -548,7 +548,7 @@ var tm = {
         //check to see if coming for a bookmarked tree
         var bookmark_id = jQuery.urlParam('tree');
         if (bookmark_id){
-            jQuery.getJSON('/trees/' + bookmark_id  + '/',
+            jQuery.getJSON(tm_static + '/trees/' + bookmark_id  + '/',
                {'format' : 'json'},
                 tm.display_tree_details);
             }
@@ -565,7 +565,7 @@ var tm = {
             window.clearTimeout(tm.clckTimeOut); 
             tm.clckTimeOut = null; 
             var spp = jQuery.urlParam('species');
-            jQuery.getJSON('/trees/location/',
+            jQuery.getJSON(tm_static + '/trees/location/',
               {'lat': olLonlat.lat, 'lon' : olLonlat.lon, 'format' : 'json', 'species':spp},
             tm.display_tree_details);
         } 
@@ -681,7 +681,7 @@ var tm = {
         
         //load in favorite trees
         var url = ['/trees/favorites/' + user + '/geojson/']
-        $.getJSON(url, function(json){
+        $.getJSON(tm_static + url, function(json){
             $.each(json, function(i,f){
                 var coords = f.coords;
                 var ll = new OpenLayers.LonLat(coords[0], coords[1]);
@@ -705,7 +705,7 @@ var tm = {
         //load in new trees
         if (user) {url = ['/trees/new/' + user + '/geojson/']}
         else {url = ['/trees/new/geojson/']}
-        $.getJSON(url, function(json){
+        $.getJSON(tm_static + url, function(json){
             $.each(json, function(i,f){
                 var coords = f.coords;
                 var ll = new OpenLayers.LonLat(coords[0], coords[1]);
@@ -767,7 +767,7 @@ var tm = {
         tm.map.events.register('click', tm.map, function(e){
             var mapCoord = tm.map.getLonLatFromViewPortPx(e.xy);
             mapCoord.transform(tm.map.getProjectionObject(), new OpenLayers.Projection("EPSG:4326"));
-            jQuery.getJSON('/trees/location/',
+            jQuery.getJSON(tm_static + '/trees/location/',
                 {'lat': mapCoord.lat, 'lon' : mapCoord.lon, 'format' : 'json', 'max_trees' : 1},
                 function(json) {
                     var html = '<a href="/trees/' + json.features[0].properties.id + '">Tree #' + json.features[0].properties.id + '</a>';
@@ -823,7 +823,7 @@ var tm = {
     load_nearby_trees : function(ll){
         //load in nearby trees as well
         var url = ['/trees/location/?lat=',ll.lat,'&lon=',ll.lon,'&format=json&max_trees=70'].join('');
-        $.getJSON(url, function(geojson){
+        $.getJSON(tm_static + url, function(geojson){
             $.each(geojson.features, function(i,f){
                 coords = f.geometry.coordinates;
                 var ll = new OpenLayers.LonLat(coords[0], coords[1]).transform(new OpenLayers.Projection("EPSG:4326"), tm.map.getProjectionObject());
@@ -953,7 +953,7 @@ var tm = {
                 if ($('#nearby_trees')) {
                     $('#nearby_trees').html("Loading...")
                     var url = ['/trees/location/?lat=',ll.lat,'&lon=',ll.lon,'&format=json&max_trees=10&distance=.0001'].join('');
-                    $.getJSON(url, function(geojson){
+                    $.getJSON(tm_static + url, function(geojson){
                         if (geojson.features.length == 0) {
                             $('#nearby_trees').html("No other trees nearby.")
                         }
@@ -1003,7 +1003,7 @@ var tm = {
                 if ($("#geocode_address")) {
                     $("#geocode_address").html("<b>Address Found: </b><br>" + results[0].formatted_address);
                     var url = ['/trees/location/?lat=',ll.lat,'&lon=',ll.lon,'&format=json&distance=20'].join('');
-                    $.getJSON(url, function(geojson){
+                    $.getJSON(tm_static + url, function(geojson){
                         $.each(geojson.features, function(i,f){
                             alert("trees");
                             //TODO: add each tree to list
@@ -1117,7 +1117,7 @@ var tm = {
                             var jsonString = JSON.stringify(data);
 
                             $.ajax({
-                                url: '/trees/location/update/',
+                                url: tm_static + '/trees/location/update/',
                                 type: 'POST',
                                 data: jsonString,
                                 complete: function(xhr, textStatus) {
@@ -1313,7 +1313,7 @@ var tm = {
     // unused?
     select_species : function(species){
         tm.mgr.clearMarkers();
-        jQuery.getJSON('/search/' + species + '/?simple=true', 
+        jQuery.getJSON(tm_static + '/search/' + species + '/?simple=true', 
             tm.display_search_results);
     },
 
@@ -1442,7 +1442,7 @@ var tm = {
             var jsonString = JSON.stringify(data);
             settings.obj = this;
             $.ajax({
-                url: '/update/',
+                url: tm_static + '/update/',
                 type: 'POST',
                 data: jsonString,
                 complete: function(xhr, textStatus) {
@@ -1491,7 +1491,7 @@ var tm = {
         };
         var jsonString = JSON.stringify(data);
         $.ajax({
-            url: '/update/',
+            url: tm_static +'/update/',
             type: 'POST',
             data: jsonString,
             complete: function(xhr, textStatus) {
@@ -1774,7 +1774,7 @@ var tm = {
         jQuery('#displayResults').show();
         //TODO: send a geoserver CQL request also
         $.ajax({
-            url: '/search/'+qs,
+            url: tm_static + '/search/'+qs,
             dataType: 'json',
             success: function(results) {
                 tm.display_search_results(results)
@@ -1842,7 +1842,7 @@ var tm = {
         $('a.favorite.fave').live('click', function(e) {
             var pk = $(this).attr('id').replace('favorite_', '');
             var url = base_create + pk + '/';
-            $.getJSON(url, function(data, textStatus) {
+            $.getJSON(tm_static + url, function(data, textStatus) {
                 $('#favorite_' + pk).removeClass('fave').addClass('unfave').text('Remove as favorite');
             });
             tm.trackEvent('Favorite', 'Add Favorite', 'Tree', pk);
@@ -1851,7 +1851,7 @@ var tm = {
         $('a.favorite.unfave').live('click', function(e) {
             var pk = $(this).attr('id').replace('favorite_', '');
             var url = base_delete + pk + '/';
-            $.getJSON(url, function(data, textStatus) {
+            $.getJSON(tm_static + url, function(data, textStatus) {
                 $('#favorite_' + pk).removeClass('unfave').addClass('fave').text('Add as favorite');
             });
             tm.trackEvent('Favorite', 'Remove Favorite', 'Tree', pk);
@@ -1865,7 +1865,7 @@ var tm = {
         //possible zipcode 
         tm.geocode_address = search;
         if (tm.isNumber(search)) {
-            jQuery.getJSON('/zipcodes/', {format:'json', name: tm.geocode_address}, function(zips){
+            jQuery.getJSON(tm_static + '/zipcodes/', {format:'json', name: tm.geocode_address}, function(zips){
                 if (tm.location_marker) {tm.misc_markers.removeMarker(tm.location_marker)} 
                             
                 if (zips.features.length > 0) {
@@ -1889,7 +1889,7 @@ var tm = {
         }
         else
         {
-            jQuery.getJSON('/neighborhoods/', {format:'json', name: tm.geocode_address}, function(nbhoods){
+            jQuery.getJSON(tm_static + '/neighborhoods/', {format:'json', name: tm.geocode_address}, function(nbhoods){
                 if (tm.location_marker) {tm.misc_markers.removeMarker(tm.location_marker)} 
 
                 if (nbhoods.features.length > 0) {
@@ -2005,7 +2005,7 @@ var tm = {
     },
     approvePend: function(pend_id) {
         $.ajax({
-            url: '/trees/pending/' + pend_id + '/approve/',
+            url: tm_static + '/trees/pending/' + pend_id + '/approve/',
             dataType: 'json',
             type: 'POST',
             success: function(response) {
@@ -2019,7 +2019,7 @@ var tm = {
     },
     rejectPend: function(pend_id) {
         $.ajax({
-            url: '/trees/pending/' + pend_id + '/reject/',
+            url: tm_static + '/trees/pending/' + pend_id + '/reject/',
             dataType: 'json',
             type: 'POST',
             success: function(response) {
@@ -2035,7 +2035,7 @@ var tm = {
         if (window.confirm("Are you sure you want to delete this tree permanently from the system?"))
         {
             $.ajax({
-                url: '/trees/' + tree_id + '/delete/',
+                url: tm_static + '/trees/' + tree_id + '/delete/',
                 dataType: 'json',
                 type: 'POST',
                 success: function(response) {
@@ -2053,7 +2053,7 @@ var tm = {
         if (window.confirm("Are you sure you want to delete this photo permanently from the system?"))
         {
             $.ajax({
-                url: '/trees/' + tree_id + '/deletephoto/' +  photo_id,
+                url: tm_static + '/trees/' + tree_id + '/deletephoto/' +  photo_id,
                 dataType: 'json',
                 type: 'POST',
                 success: function(response) {
@@ -2070,7 +2070,7 @@ var tm = {
         if (window.confirm("Are you sure you want to delete this photo permanently from the system?"))
         {
             $.ajax({
-                url: '/profiles/' + username + '/deletephoto/',
+                url: tm_static + '/profiles/' + username + '/deletephoto/',
                 dataType: 'json',
                 type: 'POST',
                 success: function(response) {
@@ -2089,7 +2089,7 @@ var tm = {
     
     updateReputation: function(change_type, change_id, rep_dir) {
         $.ajax({
-        url: '/verify/' + change_type + '/' + change_id + '/' + rep_dir,
+        url: tm_static + '/verify/' + change_type + '/' + change_id + '/' + rep_dir,
         dataType: 'json',
         success: function(response) {
             $("#" + response.change_type + "_" + response.change_id).fadeOut();
@@ -2109,7 +2109,7 @@ var tm = {
         var jsonString = JSON.stringify(data);
         
         $.ajax({
-        url: '/users/update/',
+        url: tm_static + '/users/update/',
         dataType: 'json',
         data: jsonString,
         type: 'POST',
@@ -2129,7 +2129,7 @@ var tm = {
             var jsonString = JSON.stringify(data);
             
         $.ajax({
-            url: '/users/update/',
+            url: tm_static + '/users/update/',
             dataType: 'json',
             data: jsonString,
             type: 'POST',
@@ -2150,7 +2150,7 @@ var tm = {
         var jsonString = JSON.stringify(data);
 
         $.ajax({
-            url: '/users/ban/',
+            url: tm_static + '/users/ban/',
             dataType: 'json',
             data: jsonString,
             type: 'POST',
@@ -2171,7 +2171,7 @@ var tm = {
         var jsonString = JSON.stringify(data);
 
         $.ajax({
-            url: '/users/activate/',
+            url: tm_static + '/users/activate/',
             dataType: 'json',
             data: jsonString,
             type: 'POST',
@@ -2188,7 +2188,7 @@ var tm = {
 
     updatePend: function(pend_id, pend_dir) {
         $.ajax({
-        url: '/trees/pending/' + pend_id + '/' + pend_dir,
+        url: tm_static + '/trees/pending/' + pend_id + '/' + pend_dir,
         dataType: 'json',
         success: function(response) {
             $("#" + response.pend_id).hide();
@@ -2206,7 +2206,7 @@ var tm = {
         };
         var jsonString = JSON.stringify(data);      
         $.ajax({
-            url: '/watch/validate/',
+            url: tm_static + '/watch/validate/',
             dataType: 'json',
             data: jsonString,
             type: 'POST',
@@ -2225,7 +2225,7 @@ var tm = {
         };
         var jsonString = JSON.stringify(data);      
         $.ajax({
-            url: '/comments/hide/',
+            url: tm_static + '/comments/hide/',
             dataType: 'json',
             data: jsonString,
             type: 'POST',
@@ -2243,7 +2243,7 @@ var tm = {
         };
         var jsonString = JSON.stringify(data);      
         $.ajax({
-            url: '/comments/unflag/',
+            url: tm_static + '/comments/unflag/',
             dataType: 'json',
             data: jsonString,
             type: 'POST',
