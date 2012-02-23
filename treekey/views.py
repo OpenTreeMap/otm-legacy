dfd8e9ac0eb05940cf0e23ec6a05d064b539e384
Carissa Brittain <AZVA-INT\cbrittain@dublin.(none)>
2012-02-23 13:00:45 -0500

    Fixed treekey render bug and image links

--- treekey/views.py
+++ treekey/views.py
@@ -1,3 +1,4 @@
+from django.template import RequestContext
 from django.http import HttpResponse
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import Context, Template
@@ -39,23 +40,23 @@ def trail_maker(n):
 # Returns the starting node for the key. Must be the first record in the database.
 def first_node(request):
     f = Node.objects.order_by('id')[0]
-    return render_to_response('treekey/node.html', {'request': request, 'node':f, 'display':index_maker(), 'trail':trail_maker(f)})
+    return render_to_response('treekey/node.html', RequestContext(request, {'request': request, 'node':f, 'display':index_maker(), 'trail':trail_maker(f)}))
 
 # Returns a node or leaf depending on the child objects
 def node(request, node_id):
     n = get_object_or_404(Node, pk=node_id)
     if n.has_children():
-        return render_to_response('treekey/node.html', {'request': request, 'node':n, 'display':index_maker(), 'trail':trail_maker(n)})
-    return render_to_response('treekey/leaf.html', {request: request, 'leaf':n, 'display':index_maker(), 'trail':trail_maker(n)})    
+        return render_to_response('treekey/node.html', RequestContext(request, {'request': request, 'node':n, 'display':index_maker(), 'trail':trail_maker(n)}))
+    return render_to_response('treekey/leaf.html', RequestContext(request, {'request': request, 'leaf':n, 'display':index_maker(), 'trail':trail_maker(n)})    )
 
 # Returns species information with parent node trail/index information
 def species(request, species_id):
     s = get_object_or_404(Species, pk=species_id)
     n = s.node_set.all()[0];
-    return render_to_response('treekey/species.html', {'request': request, 'species':s, 'display':index_maker(), 'trail':trail_maker(n)})
+    return render_to_response('treekey/species.html', RequestContext(request, {'request': request, 'species':s, 'display':index_maker(), 'trail':trail_maker(n)}))
     
 def browse(request):
     f = Node.objects.order_by('id')[0]
     species = Species.objects.all().order_by('common_name')
-    return render_to_response('treekey/browse.html', {'request': request, 'species':species, 'display':index_maker(), 'trail':trail_maker(f)})
+    return render_to_response('treekey/browse.html', RequestContext(request, {'request': request, 'species':species, 'display':index_maker(), 'trail':trail_maker(f)}))
     
