OpenTreeMap/otm-legacy

commit 7fcfefe88a594f75c34bfb2e266ab260ed8ee42f

Fix to update_plot view to correctly handle pending
The view determines whether to create a pend or a real edit by checking the
audit trail of the plot for a record of type 'I' (insert). An upgraded
database may not have audit insert records for plots, so I am assuming to
treat the plot as if it was create by and administrator if there is no
insert audit record.

treemap/views.py
@@ -986,7 +986,12 @@ def object_update(request):
                 
                     # if the tree was added by the public, or the current user is not public, skip pending
                 if settings.PENDING_ON and (post['model'] == "Tree" or post['model'] == "Plot"):
-                    insert_event_mgmt = instance.history.filter(_audit_change_type='I')[0].last_updated_by.has_perm('auth.change_user')
+                    audit_insert_records = plot.history.filter(_audit_change_type='I')
+                    if len(audit_insert_records) > 0:
+                        insert_event_mgmt = audit_insert_records[0].last_updated_by.has_perm('auth.change_user')
+                    else:
+                        insert_event_mgmt = True # If the insert audit record is missing, assume it was created by a manager
+
                     mgmt_user = request.user.has_perm('auth.change_user')
                     if insert_event_mgmt and not mgmt_user:
                         for k,v in update.items():
@@ -1326,7 +1331,11 @@ def update_plot(request, plot_id):
 
         if settings.PENDING_ON :
             # if the tree was added by the public, or the current user is not public, skip pending
-            insert_event_mgmt = plot.history.filter(_audit_change_type='I')[0].last_updated_by.has_perm('auth.change_user')
+            audit_insert_records = plot.history.filter(_audit_change_type='I')
+            if len(audit_insert_records) > 0:
+                insert_event_mgmt = audit_insert_records[0].last_updated_by.has_perm('auth.change_user')
+            else:
+                insert_event_mgmt = True # If the insert audit record is missing, assume it was created by a manager
             mgmt_user = request.user.has_perm('auth.change_user')
             if insert_event_mgmt and not mgmt_user:
                 # Get a clean plot object

commit 337595152c654c06557d389422cf965a40723b6a

fix type caused in commit 7fcfefe

treemap/views.py
@@ -986,7 +986,7 @@ def object_update(request):
                 
                     # if the tree was added by the public, or the current user is not public, skip pending
                 if settings.PENDING_ON and (post['model'] == "Tree" or post['model'] == "Plot"):
-                    audit_insert_records = plot.history.filter(_audit_change_type='I')
+                    audit_insert_records = instance.history.filter(_audit_change_type='I')
                     if len(audit_insert_records) > 0:
                         insert_event_mgmt = audit_insert_records[0].last_updated_by.has_perm('auth.change_user')
                     else: