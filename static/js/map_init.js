57c65ecdb3087ea6cef0ae32a24430bd5d4d0a70
Carissa Brittain <cbrittain@azavea.com>
2012-09-04 12:13:50 -0400

    Added not available note for no streetview instead of hiding

--- static/js/map_init.js
+++ static/js/map_init.js
@@ -515,7 +515,7 @@ tm.load_streetview = function(ll, div){
             
         }
         else {
-            $(div).hide()
+            $(div).html("<div class='no_streetview'>Streetview is not available at this location.</div>");
         }
     });       
     
