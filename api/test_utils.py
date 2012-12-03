fcf4be9bd7b76a804e305a874b382a9c693f091a
Adam Hinz <hinz.adam@gmail.com>
2012-12-03 14:41:39 +0000

    Refactor tests to share init code
    Previously treemap and api had a totally different set of
    initialization functions. This commit unifies them under the
    api.
    
    It also removes the really annoying initial_data.json which
    was breaking some tests locally.

--- api/test_utils.py
+++ api/test_utils.py
@@ -1,4 +1,7 @@
 from django.conf import settings
+
+settings.POSTAL_CODE_FIELD = None # Use US Zip Code
+
 from django.contrib.auth.models import User
 from django.contrib.gis.geos.collections import MultiPolygon
 from django.contrib.gis.geos.point import Point
@@ -8,6 +11,7 @@ from api.models import APILog, APIKey
 from profiles.models import UserProfile
 from treemap.models import Species, BenefitValues, Resource, Neighborhood, ZipCode, ExclusionMask, AggregateNeighborhood, ImportEvent, Tree, Plot
 
+import django.shortcuts
 
 def mkPlot(u, geom=Point(50,50)):
     p = Plot(geometry=geom, last_updated_by=u, import_event=ImportEvent.objects.all()[0],present=True, data_owner=u)
@@ -15,16 +19,17 @@ def mkPlot(u, geom=Point(50,50)):
 
     return p
 
-def mkTree(u, plot=None, species=None):
+def mkTree(u, plot=None, species=-1):
     if not plot:
         plot = mkPlot(u)
 
-    if species:
-        s = species
-    else:
+    if species == -1:
         s = Species.objects.all()[0]
+    else:
+        s = species
 
     t = Tree(plot=plot, species=s, last_updated_by=u, import_event=ImportEvent.objects.all()[0])
+    t.present = True
     t.save()
 
     return t
@@ -34,31 +39,56 @@ def setupTreemapEnv():
     settings.GEOSERVER_GEO_STYLE = ""
     settings.GEOSERVER_URL = ""
 
+    def local_render_to_response(*args, **kwargs):
+        from django.template import loader, RequestContext
+        from django.http import HttpResponse
+        
+        httpresponse_kwargs = {'mimetype': kwargs.pop('mimetype', None)}
+        hr = HttpResponse(
+            loader.render_to_string(*args, **kwargs), **httpresponse_kwargs)
+
+        if hasattr(args[1], 'dicts'):
+            hr.request_context = args[1].dicts
+
+        return hr
+
+    django.shortcuts.render_to_response = local_render_to_response
+
     r1 = ReputationAction(name="edit verified", description="blah")
     r2 = ReputationAction(name="edit tree", description="blah")
     r3 = ReputationAction(name="Administrative Action", description="blah")
     r4 = ReputationAction(name="add tree", description="blah")
     r5 = ReputationAction(name="edit plot", description="blah")
     r6 = ReputationAction(name="add plot", description="blah")
+    r7 = ReputationAction(name="add stewardship", description="blah")
+    r8 = ReputationAction(name="remove stewardship", description="blah")
 
-    for r in [r1,r2,r3,r4,r5,r6]:
+    for r in [r1,r2,r3,r4,r5,r6,r7,r8]:
         r.save()
 
     bv = BenefitValues(co2=0.02, pm10=9.41, area="InlandValleys",
-        electricity=0.1166,voc=4.69,ozone=5.0032,natural_gas=1.25278,
-        nox=12.79,stormwater=0.0078,sox=3.72,bvoc=4.96)
+                       electricity=0.1166,voc=4.69,ozone=5.0032,natural_gas=1.25278,
+                       nox=12.79,stormwater=0.0078,sox=3.72,bvoc=4.96)
 
     bv.save()
 
-    dbh = "[1.0, 2.0, 3.0]"
+    dbh = "[1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0]"
+    dbh2 = "[2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0]"
 
-    rsrc = Resource(meta_species="BDM_OTHER", electricity_dbh=dbh, co2_avoided_dbh=dbh,
-        aq_pm10_dep_dbh=dbh, region="Sim City", aq_voc_avoided_dbh=dbh,
-        aq_pm10_avoided_dbh=dbh, aq_ozone_dep_dbh=dbh, aq_nox_avoided_dbh=dbh,
-        co2_storage_dbh=dbh,aq_sox_avoided_dbh=dbh, aq_sox_dep_dbh=dbh,
-        bvoc_dbh=dbh, co2_sequestered_dbh=dbh, aq_nox_dep_dbh=dbh,
-        hydro_interception_dbh=dbh, natural_gas_dbh=dbh)
-    rsrc.save()
+    rsrc1 = Resource(meta_species="BDM_OTHER", electricity_dbh=dbh, co2_avoided_dbh=dbh,
+                     aq_pm10_dep_dbh=dbh, region="Sim City", aq_voc_avoided_dbh=dbh,
+                     aq_pm10_avoided_dbh=dbh, aq_ozone_dep_dbh=dbh, aq_nox_avoided_dbh=dbh,
+                     co2_storage_dbh=dbh,aq_sox_avoided_dbh=dbh, aq_sox_dep_dbh=dbh,
+                     bvoc_dbh=dbh, co2_sequestered_dbh=dbh, aq_nox_dep_dbh=dbh,
+                     hydro_interception_dbh=dbh, natural_gas_dbh=dbh)
+    rsrc2 = Resource(meta_species="BDL_OTHER", electricity_dbh=dbh2, co2_avoided_dbh=dbh2,
+                    aq_pm10_dep_dbh=dbh2, region="Sim City", aq_voc_avoided_dbh=dbh2,
+                    aq_pm10_avoided_dbh=dbh2, aq_ozone_dep_dbh=dbh2, aq_nox_avoided_dbh=dbh2,
+                    co2_storage_dbh=dbh2,aq_sox_avoided_dbh=dbh2, aq_sox_dep_dbh=dbh2,
+                    bvoc_dbh=dbh2, co2_sequestered_dbh=dbh2, aq_nox_dep_dbh=dbh2,
+                    hydro_interception_dbh=dbh2, natural_gas_dbh=dbh2)
+    rsrc1.save()
+    rsrc2.save()
 
     u = User.objects.filter(username="jim")
 
@@ -79,13 +109,13 @@ def setupTreemapEnv():
         amy = User.objects.create_user("amy","amy@test.org","amy")
     else:
         amy = amy_filter_result[0]
-    amy.is_staff = False
-    amy.is_superuser = False
-    amy.save()
-    amy_profile = UserProfile(user=amy)
-    amy_profile.save()
-    amy.reputation = Reputation(user=amy)
-    amy.reputation.save()
+        amy.is_staff = False
+        amy.is_superuser = False
+        amy.save()
+        amy_profile = UserProfile(user=amy)
+        amy_profile.save()
+        amy.reputation = Reputation(user=amy)
+        amy.reputation.save()
 
     n1geom = MultiPolygon(Polygon(((0,0),(100,0),(100,100),(0,100),(0,0))))
     n2geom = MultiPolygon(Polygon(((0,101),(101,101),(101,200),(0,200),(0,101))))
@@ -155,9 +185,28 @@ def setupTreemapEnv():
 
     s1 = Species(symbol="s1",genus="testus1",species="specieius1")
     s2 = Species(symbol="s2",genus="testus2",species="specieius2")
+    s3 = Species(symbol="s3",genus="testus2",species="specieius3")
+
+    s1.native_status = 'True'
+    s1.fall_conspicuous = True
+    s1.flower_conspicuous = True
+    s1.palatable_human = True
+
+    s2.native_status = 'True'
+    s2.fall_conspicuous = False
+    s2.flower_conspicuous = True
+    s2.palatable_human = False
+    s2.wildlife_value = True
+
+    s3.wildlife_value = True
 
     s1.save()
     s2.save()
+    s3.save()
+
+    s1.resource.add(rsrc1)
+    s2.resource.add(rsrc2)
+    s3.resource.add(rsrc2)
 
     ie = ImportEvent(file_name='site_add')
     ie.save()
@@ -200,4 +249,7 @@ def teardownTreemapEnv():
         r.delete()
 
     for r in ReputationAction.objects.all():
+        r.delete()
+
+    for r in Resource.objects.all():
         r.delete()
