6905ceee482fec564d76ba4d14e5c98bfee0518d
Adam Hinz <hinz.adam@gmail.com>
2012-12-11 16:25:53 +0000

    A bit overzealous on this... forgot to include the params

--- treemap/localization.py
+++ treemap/localization.py
@@ -3,10 +3,10 @@ from django.conf import settings
 def PostalCodeField(*args, **kwargs):
     if settings.POSTAL_CODE_FIELD == "GBPostcodeField":
         from django.contrib.localflavor.uk.forms import UKPostcodeField
-        return UKPostcodeField
+        return UKPostcodeField(*args, **kwargs)
     else:
         from django.contrib.localflavor.us.forms import USZipCodeField
-        return USZipCodeField
+        return USZipCodeField(*args, **kwargs)
 
 def convert_dbh_to_inches(dbh):
     return dbh * settings.DBH_TO_INCHES_FACTOR
