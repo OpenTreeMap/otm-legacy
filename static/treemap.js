OpenTreeMap/otm-legacy

commit 9f38bc0d0f7a2cfd75a7cb8a5ecc3495bf510670

Adding cultivar to species list dropdown.
Also adds 'single_quote' template tag. This template tag is currently
only used in the TreeZilla skin.

static/treemap.js
@@ -403,6 +403,7 @@ tm = {
                     });
                 } else {
                     var value = response['update'][settings.fieldName];
+                    var datum;
                     
                     if (!value) {
                         value = response['update']['value'];
@@ -410,6 +411,7 @@ tm = {
                     if (settings.fieldName == "species_id") {
                         for (var i = 0; i < tm.speciesData.length; i++) {
                             if (tm.speciesData[i].id == value) {
+                                datum = tm.speciesData[i];
                                 value = tm.speciesData[i].sname;
                                 $("#edit_species").html(tm.speciesData[i].cname);
                             }
@@ -425,7 +427,7 @@ tm = {
                     if (settings.fieldName == "plot_width" || settings.fieldName == "plot_length") {
                         if (value == 99.0) {value = "15+"}
                     }
-                    settings.obj.innerHTML = value 
+                    settings.obj.innerHTML = tm.formatSpeciesName(datum)
                     tm.trackEvent("Edit", settings.fieldName)
                 }
             }});
@@ -481,6 +483,16 @@ tm = {
             }});
     },
 
+    formatTreeName: function(item) {
+        var cultivar_portion = item.cultivar ? item.cultivar + " " : "";
+        return item.cname + " [ " + item.sname + " " + cultivar_portion + "]";
+    },
+
+    formatSpeciesName: function(item) {
+        var cultivar_portion = item.cultivar ? " '" + item.cultivar + "'" : "";
+        return item.sname + cultivar_portion;
+    },
+
     setupAutoComplete: function(field) {
         return field.autocomplete({
             source:function(request, response){
@@ -489,7 +501,7 @@ tm = {
                         item.sname.toLowerCase().indexOf(request.term.toLowerCase()) != -1) 
                     {
 					    return {
-						    label: item.cname + " [ " + item.sname + " ]",
+						    label: tm.formatTreeName(item),
 						    value: item.id
 					    }
                     }

static/js/page_init.js
@@ -284,9 +284,8 @@ tm.generateSpeciesDropdown = function(speciesData) {
         if (speciesData[i].count == 0) {continue;}
         var c = "ac_odd";
         if (i%2 == 0) {c = 'ac-even';}
-        ul.append("<li id='" + speciesData[i].id + "' class='" + c + "'>" + speciesData[i].cname + " [" + speciesData[i].sname + "]</li>")
+        ul.append("<li id='" + speciesData[i].id + "' class='" + c + "'>" + tm.formatTreeName(speciesData[i]) + "</li>")
     }
-    
     $("#s_list > li").hover(function(evt) {
         $(this).addClass("ac_over")
     }, function(evt) {

treemap/templatetags/tree_tags.py
@@ -44,3 +44,10 @@ def unit_or_zero(value, unit=None):
             return ("%.2f" % float(value)) + " " + unit
         return "%.2f" % float(value)
     return "%.2f" % 0.00
+
+@register.filter
+def single_quote(value):
+    if value:
+        return "'" + value + "'"
+    else:
+        return ""

commit 6d22018d91ae6229070cbbe32a5920481d83dd49

Fix bug introduced in 9f38bc0d0f
The species cultivar formatter was being called on all updates
not just on species updates

static/treemap.js
@@ -403,7 +403,6 @@ tm = {
                     });
                 } else {
                     var value = response['update'][settings.fieldName];
-                    var datum;
                     
                     if (!value) {
                         value = response['update']['value'];
@@ -411,8 +410,7 @@ tm = {
                     if (settings.fieldName == "species_id") {
                         for (var i = 0; i < tm.speciesData.length; i++) {
                             if (tm.speciesData[i].id == value) {
-                                datum = tm.speciesData[i];
-                                value = tm.speciesData[i].sname;
+                                value = tm.formatSpeciesName(tm.speciesData[i]);
                                 $("#edit_species").html(tm.speciesData[i].cname);
                             }
                         }
@@ -423,11 +421,12 @@ tm = {
                         } else {
                             $("#edit_species").append('<br>' + other1 + " " + other2);
                         }
+
                     }
                     if (settings.fieldName == "plot_width" || settings.fieldName == "plot_length") {
                         if (value == 99.0) {value = "15+"}
                     }
-                    settings.obj.innerHTML = tm.formatSpeciesName(datum)
+                    settings.obj.innerHTML = value;
                     tm.trackEvent("Edit", settings.fieldName)
                 }
             }});