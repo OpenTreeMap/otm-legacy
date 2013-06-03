5f14ba982ea8543c35f1d9a3161ef8c5521f3adf
Andrew Thompson <athompson@azavea.com>
2013-06-03 13:36:40 -0400

    adding choices.example.py

---
+++ choices.example.py
@@ -0,0 +1,67 @@
+CHOICES = {
+            'alerts': [
+                ("1", "Needs Watering"), 
+                ("2", "Needs Pruning"), 
+                ("3", "Should be Removed"), 
+                ("4", "Pest Or Disease Present"), 
+                ("5", "Guard Should Be Removed"), 
+                ("6", "Stakes And Ties Should Be Removed"), 
+                ("7", "Construction Work In The Area"), 
+                ("8", "Touching Wires"), 
+                ("9", "Blocking Signs or Traffic Signals"), 
+                ("10", "Improperly Pruned or Topped")
+            ],
+            'actions': [
+            ],
+            'projects': [
+            ],
+            'sidewalks': [
+                ("1", "Minor or No Damage"),
+                ("2", "Raised More Than 3/4 Inch")
+            ],
+            'powerlines': [
+                ("1", "Yes"),
+                ("2", "No"),
+                ("3", "Unknown")
+            ],
+            'conditions': [
+                ("1", "Dead"),
+                ("2", "Critical"),
+                ("3", "Poor"),
+                ("4", "Fair"),
+                ("5", "Good"),
+                ("6", "Very Good"),
+                ("7", "Excellent")
+            ],
+            'canopy_conditions': [
+                ("1", "Full - No Gaps"),
+                ("2", "Small Gaps (up to 25% missing)"),
+                ("3", "Moderate Gaps (up to 50% missing)"),
+                ("4", "Large Gaps (up to 75% missing)"),
+                ("5", "Little or None (up to 100% missing)")
+            ],
+            'plot_types': [
+                ("1", "Well/Pit"),
+                ("2", "Median/Island"),
+                ("3", "Tree Lawn"),
+                ("4", "Park"),
+                ("5", "Planter"),
+                ("6", "Other"),
+                ("7", "Yard"),
+                ("8", "Natural Area")
+            ],
+            'plot_stewardship': [
+                ("1","Watering"),
+                ("2","Pruning"),
+                ("3","Mulching, Adding Compost or Amending Soil"),
+                ("4","Removing Debris or Trash")
+            ],
+            'tree_stewardship': [
+                ("1","Enlarging the Planting Area"),
+                ("2","Adding a Guard"),
+                ("3","Removing a Guard"),
+                ("4","Herbaceous Planting")
+            ]
+    }
+
+
